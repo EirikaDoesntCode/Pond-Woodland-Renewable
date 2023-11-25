f8af71766417b4083c617ef506eeedd4ade5e76b
EirikaDoesntCode <eirikadoesntcode@gmail.com>
2023-11-25 12:17:30 +0000

    Some buttons and label: layout

---
+++ world.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var egg_meter_perc;
+	var fish_count;
+	var wood_count;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
