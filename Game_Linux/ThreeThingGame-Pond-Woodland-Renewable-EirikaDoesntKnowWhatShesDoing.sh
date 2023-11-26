79d472d3a126310c6fb6418e2bc8683d3d8d7bc9
Eirika Doe <EirikaDoesntCode@gmail.com>
2023-11-26 17:26:48 +0000

    Delete Game_Linux directory

--- Game_Linux/ThreeThingGame-Pond-Woodland-Renewable-EirikaDoesntKnowWhatShesDoing.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo -ne '\033c\033]0;ThreeThingGame-Pond-Woodland-Renewable-EirikaDoesntKnowWhatShesDoing\a'
-base_path="$(dirname "$(realpath "$0")")"
-"$base_path/ThreeThingGame-Pond-Woodland-Renewable-EirikaDoesntKnowWhatShesDoing.x86_64" "$@"
